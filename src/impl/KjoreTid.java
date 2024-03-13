e76231ca7f8f11675b0d7d54332c94da27e600a9
h676648 <142889234+h676648@users.noreply.github.com>
2024-03-13 16:24:59 +0100

    #6

--- src/impl/KjoreTid.java
+++ src/impl/KjoreTid.java
@@ -3,20 +3,24 @@ package impl;
 public class KjoreTid {
     public static void main(String[] args) {
 
-        TabellMengde<Integer> tabellMengde = new TabellMengde<>();
+        //LenketMengde<Integer> lenketMengde2 = new LenketMengde<>();
         LenketMengde<Integer> lenketMengde = new LenketMengde<>();
-        for (int i = 0; i < 1000; i++) {
-            tabellMengde.leggTil(i);
+        for (int i = 0; i <= 100000; i++) {
             lenketMengde.leggTil(i);
         }
+
         long startTid = System.nanoTime();
-        tabellMengde.inneholder(999);
-
-
-
-
+        lenketMengde.fjern(100000);
         long sluttTid = System.nanoTime();
         long resultat = sluttTid - startTid;
-        System.out.println("Worst case " + resultat + " Nanosekund");
+        System.out.println("Best case " + resultat + " Nanosekund");
+        lenketMengde.leggTil(100000);
+
+        long startTid2 = System.nanoTime();
+        lenketMengde.fjern(0);
+        long sluttTid2 = System.nanoTime();
+        long resultat2 = sluttTid2 - startTid2;
+        System.out.println("Worst case " + resultat2 + " Nanosekund");
+
     }
 }
