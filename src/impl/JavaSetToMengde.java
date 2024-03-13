774585ad1ffa45ddcab7f3b1fd86679532c1e415
h678129 <678129@stud.hvl.no>
2024-03-13 13:41:53 +0100

    #3

---
+++ src/impl/JavaSetToMengde.java
@@ -0,0 +1,119 @@
+package impl;
+
+import adt.MengdeADT;
+
+import java.util.Set;
+
+public class JavaSetToMengde<T> implements MengdeADT<T> {
+
+    private Set<T> set;
+
+    public JavaSetToMengde(){
+
+    }
+
+
+
+
+    @Override
+    public boolean erTom() {
+
+        return set.isEmpty();
+    }
+
+    @Override
+    public boolean inneholder(T element) {
+        return set.contains(element);
+    }
+
+    @Override
+    public boolean erDelmengdeAv(MengdeADT<T> annenMengde) {
+        for (T element : set){
+            if (!annenMengde.inneholder(element)) {
+                return false;
+            }
+        }
+        return true;
+
+    }
+
+    @Override
+    public boolean erLik(MengdeADT<T> annenMengde) {
+        if (set.size() == annenMengde.antallElementer()) {
+            return erDelmengdeAv(annenMengde);
+        }
+        return false;
+    }
+
+    @Override
+    public boolean erDisjunkt(MengdeADT<T> annenMengde) {
+        for (T element : set) {
+            if (annenMengde.inneholder(element)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    @Override
+    public MengdeADT<T> snitt(MengdeADT<T> annenMengde) {
+        MengdeADT<T> mengde = new JavaSetToMengde<>();
+        for (T element : set) {
+            if (annenMengde.inneholder(element)) {
+                mengde.leggTil(element);
+            }
+        }
+
+        return mengde;
+    }
+
+    @Override
+    public MengdeADT<T> union(MengdeADT<T> annenMengde) {
+        MengdeADT<T> mengde = new JavaSetToMengde<>();
+        mengde.leggTilAlleFra(annenMengde);
+        mengde.leggTilAlleFra(this);
+        return mengde;
+    }
+
+    @Override
+    public MengdeADT<T> differanse(MengdeADT<T> annenMengde) {
+        MengdeADT<T> mengde = new JavaSetToMengde<>();
+        for (T element : set) {
+            if (element.equals(annenMengde)) {
+                mengde.leggTil(element);
+            }
+        }
+        return mengde;
+    }
+
+    @Override
+    public void leggTil(T element) {
+        set.add(element);
+    }
+
+    @Override
+    public void leggTilAlleFra(MengdeADT<T> annenMengde) {
+        for (T element : annenMengde.tilTabell()) {
+            leggTil(element);
+        }
+    }
+
+    @Override
+    public T fjern(T element) {
+        if(set.contains(element)) {
+            set.remove(element);
+            return element;
+        }
+        return null;
+    }
+
+    @Override
+    public T[] tilTabell() {
+        return (T[]) set.toArray();
+    }
+
+    @Override
+    public int antallElementer() {
+        return set.size();
+    }
+}
---
+++ src/test/impl/JavaSetToMengdeTest.java
@@ -0,0 +1,60 @@
+package impl;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class JavaSetToMengdeTest {
+
+    @Test
+    void erTom() {
+    }
+
+    @Test
+    void inneholder() {
+    }
+
+    @Test
+    void erDelmengdeAv() {
+    }
+
+    @Test
+    void erLik() {
+    }
+
+    @Test
+    void erDisjunkt() {
+    }
+
+    @Test
+    void snitt() {
+    }
+
+    @Test
+    void union() {
+    }
+
+    @Test
+    void differanse() {
+    }
+
+    @Test
+    void leggTil() {
+    }
+
+    @Test
+    void leggTilAlleFra() {
+    }
+
+    @Test
+    void fjern() {
+    }
+
+    @Test
+    void tilTabell() {
+    }
+
+    @Test
+    void antallElementer() {
+    }
+}
